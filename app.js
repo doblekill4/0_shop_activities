59f4de7c0b60189157b8844ba5feba984d1e45f8
doblekill4 <876056372@qq.com>
2026-07-18 12:36:28 +0800

    chore: bump to v1.0.3, update README changelog

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ App({
     userInfo: null,     // { _id, name, department, permissions[], role }
     loginReady: false,  // 登录检测是否完成
     // role: 'admin' | 'manager' | 'user'
-    appVersion: '1.0.1',
+    appVersion: '1.0.3',
     // 权限标识
     PERMISSIONS: {
       CREATE_ACTIVITY: 'create_activity',
